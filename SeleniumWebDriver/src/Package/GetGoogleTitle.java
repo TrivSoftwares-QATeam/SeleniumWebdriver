dc0e301ee5c0efe0c3efb88116070b4d1542644b
sarath <sarath@AI-QC-01.TrivSoft.com>
2017-06-16 12:46:56 +0530

    Added the comment //This is a sample program to get the title of Google page // in class "GetGoogleTitle". Also added the code to print the Google Title

--- SeleniumWebDriver/src/Package/GetGoogleTitle.java
+++ SeleniumWebDriver/src/Package/GetGoogleTitle.java
@@ -1,4 +1,4 @@
-//Testing added by sreejith
+//This is a sample program to get the title of Google page //
 package Package;
 
 import java.util.concurrent.TimeUnit;
@@ -7,16 +7,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class GetGoogleTitle {
+public class GetGoogleTitle 
+{
 	
 	@Test
-	public static void main(String[] args) 
+	public void GetGoogleTitle() 
 	{
 		System.setProperty("webdriver.chrome.driver", "D:\\_________SELENIUM_WEBDRIVER\\chromedriver.exe");	
 	    WebDriver driver = new ChromeDriver();
-	    //driver = new FirefoxDriver();
 	    driver.get("http://www.google.co.in/");
 	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    System.out.println(driver.getTitle());
+	    driver.quit();
 	}
 
 }
